a7a04a4b0cf515e8325ffdadb111c29d42f83e42
Kha <Administrator@PC028>
2019-03-28 11:18:14 +0700

    Initial

--- BigSchool/Models/Category.cs
+++ BigSchool/Models/Category.cs
@@ -5,8 +5,11 @@ namespace BigSchool.Models
     public class Category
     {
         public byte Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+
     }
-    [Required]
-    [StringLength(255)]
-    public string Name { get; set; }
+
 }
